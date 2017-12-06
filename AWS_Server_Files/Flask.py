908e015fdf36b663e2e1f374e6d7f93f0b288f1e
Ong_Laptop <ong.weng90@gmail.com>
2017-12-06 11:44:09 -0800

    update

--- AWS_Server_Files/Flask.py
+++ AWS_Server_Files/Flask.py
@@ -42,11 +42,13 @@ def deticon(c):
     return iconpaths.get(c,"/static/mapicons/greendot12px.png")
 
 #set1=[[37.4500,-122.1350,6],[37.4400,-122.1350,8],[37.4300,-122.1350,10]]    
-    
+ 
+#create map objects for Flask framework(Google API) 
 @app.route('/')
 def mapview():
     centercoord=[37.337361,-121.882691]
     
+    #parse trigger data from MainTrigger.txt
     path = "MainTrigger.txt"
     set0=[]
     n=[]
@@ -59,12 +61,13 @@ def mapview():
 
     m0=[]
     m1=[]
+    #build marker dictionaries
     for i in range(len(set0)):
         imgpath="<img src=\"/static/images/"+set0[i][3]+"\" width=100px/>"
         m0.append({'lat':set0[i][0],'lng':set0[i][1],'infobox':imgpath})
         m1.append({'lat':set0[i][0],'lng':set0[i][1]})
     
-    
+    #set default circle settings on map
     label2=['stroke_color','stroke_opacity','stroke_weight','fill_color','fill_opacity','center','radius']
     c1=[]
     S_O1=1.0 #stroke opacity
@@ -73,13 +76,15 @@ def mapview():
     RAD1=7 #radius
     templist=[0,S_O1,S_W1,0,F_O1,0,RAD1]
     
+    #build required circle dictionaries
     for i in range(len(set0)):
         color = findroughcolor(set0[i][2])
         templist[0]=color #stroke_color
         templist[3]=color #fill_color
         templist[5]=m1[i] #center  
         c1.append(dict(zip(label2,templist)))
-
+    
+    #create Map object
     mymap = Map(
         identifier = "potholemap",
         varname="potholemap",
@@ -91,6 +96,7 @@ def mapview():
         style=("height:750px;" "width:80%;" "position:relative;")
 	)
     
+    #parse Roughness data
     path = "MainRoughness.txt"
     set1=[]
     n=[]
@@ -102,10 +108,12 @@ def mapview():
             set1.append(n)
 
     m2=[]
+    #build second set of marker dictionaries
     for i in range(len(set1)):
         iconpath=deticon(set1[i][2])
         m2.append({'lat':set1[i][0],'lng':set1[i][1],'icon':iconpath})
         
+     #generate roughness map object   
     sndmap = Map(
         identifier = "roughmap",
         varname="roughmap",
@@ -117,7 +125,7 @@ def mapview():
         style=("height:750px;" "width:80%;" "position:relative;")
         )
 
-    return render_template('htmlcode.html', mymap=mymap, sndmap=sndmap)
+    return render_template('htmlcode.html', mymap=mymap, sndmap=sndmap) #utilize htmlcode.html as template for website
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0', port=8000)
+    app.run(debug=True,host='0.0.0.0', port=8000) #run in debug mode (site actually utilizes nohup command will not see debug messeges)
