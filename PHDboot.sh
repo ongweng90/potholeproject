1c0e22417abdb407e829a611c7313bfc43af0fc5
ongweng90 <32526412+ongweng90@users.noreply.github.com>
2017-12-06 08:26:06 -0800

    Update PHDboot.sh

--- PHDboot.sh
+++ PHDboot.sh
@@ -11,30 +11,28 @@
 # Description: PHD script to call main scripts and handles file transfer, shuts down at the end
 ### END INIT INFO
 
-echo "begin script"
+echo "begin script" #echo's for debugging purposes
 echo "running main script"
-#/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t DrivingMode
-# /usr/bin/python2.7 /home/pi/DistanceDetector/PHDMainE.py
-/usr/bin/python2.7 /home/pi/DistanceDetector/test_k_const.py
+/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t DrivingMode #show text calls 16x2 LCD screen and display's argument
+/usr/bin/python2.7 /home/pi/DistanceDetector/PHDMainE.py
 
-#/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t SendingFiles
+/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t SendingFiles
 echo "sending files to server"
-#scp /home/pi/DistanceDetector/pic* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/static/images/
-#scp /home/pi/DistanceDetector/trigger* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/
-#scp /home/pi/DistanceDetector/rough* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/
+scp /home/pi/DistanceDetector/pic* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/static/images/
+scp /home/pi/DistanceDetector/trigger* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/
+scp /home/pi/DistanceDetector/rough* ec2-user@ec2-54-153-33-174.us-west-1.compute.amazonaws.com:/home/ec2-user/
 
-#archivepath="/home/pi/DistanceDetector/Sent_`date +%F`"
-#mkdir $archivepath
+archivepath="/home/pi/DistanceDetector/Sent_`date +%F`"
+mkdir $archivepath
 
-#/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t MovingFiles
-#echo "moving files to $archivepath"
-#mv /home/pi/DistanceDetector/pic* $archivepath
-#mv /home/pi/DistanceDetector/trigger* $archivepath
-#mv /home/pi/DistanceDetector/rough* $archivepath
+/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t MovingFiles
+echo "moving files to $archivepath"
+mv /home/pi/DistanceDetector/pic* $archivepath
+mv /home/pi/DistanceDetector/trigger* $archivepath
+mv /home/pi/DistanceDetector/rough* $archivepath
 
-#/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t SleepIn180s
-#echo "end script; waiting 60s; shutting down"
-#sleep 180
+/usr/bin/python2.7 /home/pi/DistanceDetector/showtext -t SleepIn180s
+echo "end script; waiting 60s; shutting down"
+sleep 180
 echo "shutting down"
-#sudo -S shutdown
-#configured /etc/lightdm/lightdm.conf for autologin into user pi and run this script
+sudo -S shutdown
