02cfff9abe0293677c8f13d024c0ffcfc9a73bdd
ongweng90 <32526412+ongweng90@users.noreply.github.com>
2017-12-06 08:22:36 -0800

    Update update_main.sh

--- update_main.sh
+++ update_main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#called every minute by crontab, append and move files to a Processed folder
 cat trigger* >> MainTrigger.txt
 cat roughness* >> MainRoughness.txt
 mv trigger* /home/ec2-user/Processed/
